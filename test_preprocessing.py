9f3609fd8b16bb20e7fa7231e23325f5043aee45
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-11-05 06:31:41 -0600

    new model

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
+"""Function to convert the speedProfile to Upload and Downlaod data"""
+def convert_speed(speed):
+  if speed.endswith("M"):
+    speed = float(speed.replace('M', ''))
+  elif speed.endswith("K"):
+    speed = float(speed.replace("K", ''))
+    speed /= 1000
+  else:
+    speed = float(speed.replace('G', ''))
+    speed *= 1000
+  return speed
 
-# Preprocessing function
-def preprocess_data(data):
+"""Preproecessing the test data inorder to pass into the model."""
+def test_data_convertion(data):
   data = data.drop('Customer', axis=1)
   data = data.drop('clli', axis=1)
   data = data.drop(['year', 'month', 'day', 'date'], axis=1)
@@ -24,5 +35,10 @@ def preprocess_data(data):
   data.loc[data['TMIN'] > 50, 'SNOW'] = data.loc[data['TMIN'] > 50, 'SNOW'].fillna(0.0)
   data = data.drop('SNOW', axis=1)
   data = data.dropna(axis=1)
-  data = data.drop(['status', 'ranged', 'speedProfile'], axis=1)
+  data['status'] = pd.Categorical(data['status'], ordered=True).codes
+  data['ranged'] = pd.Categorical(data['ranged'], ordered=True).codes
+  data['download_speed'] = data['speedProfile'].apply(lambda x: convert_speed(x.split('/')[0]))
+  data['upload_speed'] = data['speedProfile'].apply(lambda x: convert_speed(x.split('/')[1]))
+  # Remove the speedProfile column if needed
+  data = data.drop('speedProfile', axis=1)
   return data
